3944451866151dfeb8a44cc8f0783105e970e88c
abdullah4a <45167847+abdullah4a@users.noreply.github.com>
2021-08-05 06:52:40 +0500

    cloned the app

---
+++ src/components/admins.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>This is Admin Page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "admin",
+  data() {
+    return {};
+  },
+};
+</script>
---
+++ src/components/home.vue
@@ -0,0 +1,242 @@
+<template>
+  <div class="home">
+    <div id="nav">
+      <div class="header"><h3>List of Names</h3></div>
+      <div class="links">
+        <ul>
+          <li v-for="per in Users" :key="per.id">
+          <a @click="selectedserfun(per)"
+          :class="{ 'is-active': selectedUser === per }"
+        >
+          {{ per.firstName }}
+        </a>
+          </li>
+        </ul>
+        
+      </div>
+    </div>
+    <div v v-for="admin in Users" :key="admin.id">
+    <div id="admincontainer" v-if="selectadmin(admin)">
+      <div class="header">
+        <h4>Admin</h4>
+        <p>{{ fullName }}</p>
+        <Admin/>
+      </div>
+    </div>
+    </div>
+    <div id="usercontainer" v-if="selectedUser">
+      <div class="header">
+        <h4>User</h4>
+        <p>{{ fullName }}</p>
+      </div>
+      <div class="content">
+        <div class="field">
+          <label for="id"> User ID </label><br />
+          <input type="text" id="id" v-model="selectedUser.id" readonly />
+        </div>
+        <div class="field">
+          <label for="firstname"> First Name</label><br />
+          <input type="text" id="firstname" v-model="selectedUser.firstName" />
+        </div>
+        <div class="field">
+          <label for="lastame"> Last Name</label><br />
+          <input type="text" id="lastname" v-model="selectedUser.lastname" />
+        </div>
+        <div class="field">
+          <label for="age"> User Age </label><br />
+          <input type="text" id="age" v-model="selectedUser.age" />
+        </div>
+        <div class="btndiv">
+          <button class="btn" @click="cancelbtn">Cancel</button>
+        </div>
+        <div class="btndiv">
+          <button class="btn" @click="saveBtn">Save</button>
+        </div>
+      </div>
+    </div>
+    <div class="message">
+      <p>
+        <pre>{{ message }}</pre>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+import Admin from "./admins.vue"
+const User = [
+  {
+    id: 0,
+    firstName: "Abdul manan",
+    lastname: "Ali",
+    age: 22,
+  },
+  {
+    id: 1,
+    firstName: "Ahmad",
+    lastname: "Raza",
+    age: 22,
+  },
+  {
+    id: 2,
+    firstName: "Ali",
+    lastname: "Raza",
+    age: 22,
+  },
+  {
+    id: 2,
+    firstName: "Raza",
+    lastname: "Mowana",
+    age: 22,
+  },
+  {
+    id: 2,
+    firstName: "Shabbar",
+    lastname: "Raza",
+    age: 22,
+  },
+  {
+    id: 2,
+    firstName: "Asad ",
+    lastname: "Shafique",
+    age: 22,
+  },
+];
+export default {
+  name: "Home",
+  components:{
+Admin,
+  },
+  data() {
+    return {
+      Users:[],
+      selectedUser: undefined,
+      selectedadmin: undefined,
+      message: "This is message",
+    };
+  },
+  methods: {
+    cancelbtn() {
+      this.selectedUser=undefined;
+      this.message = "";
+    },
+    saveBtn() {
+      this.message = JSON.stringify(this.selectedUser, null, "\n");
+    },
+    async GetUsers() {
+      return new Promise((resolve) => {
+        setTimeout(() => resolve(User), 1500);
+      });
+    },
+    async LoadUsers() {
+      this.Users=[];
+      this.message = "Please wait... Users are being Loaded";
+      this.Users = await this.GetUsers();
+      this.message = "";
+    },
+    selectedserfun(persons){
+      this.selectedUser=persons;
+    },
+    selectadmin(ad){
+      this.selectedadmin=ad;
+    }
+  },
+  created() {
+    this.LoadUsers();
+  },
+  computed: {
+    fullName() {
+      if (this.selectedUser) {
+        return `${this.selectedUser.firstName} ${this.selectedUser.lastname}`;
+      } else {
+        return `${this.selectedadmin.firstName} ${this.selectedadmin.lastname}`;
+      }
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+  margin: 10px;
+  padding: 10px;
+  border-radius: 4px;
+  cursor: -webkit-grab;
+}
+.header {
+  width: auto;
+  height: auto;
+  margin: 10px;
+  padding: 2px;
+  border-radius: 4px;
+  background-color: #42b984;
+}
+.home {
+  width: 80%;
+  height: auto;
+  margin: auto;
+}
+.content {
+  background-color: lightgray;
+  margin: 10px;
+  border-radius: 4px;
+}
+.content .field {
+  padding: 2px;
+  margin: auto;
+  font-family: Georgia, serif;
+}
+label {
+  float: left;
+  margin: 20px;
+}
+input[type="text"],
+select {
+  font-size: 18px;
+  width: 95%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+}
+.is-active {
+  background-color: #42b980;
+  color: aliceblue;
+}
+.links {
+  padding: 10px;
+}
+.message {
+  border-radius: 4px;
+  margin: 10px;
+  padding: 10px;
+  text-align: left;
+  font-size: 18px;
+  background-color: rgb(29, 160, 204);
+}
+.btndiv {
+  display: inline;
+  margin: 10px;
+}
+.btn {
+  padding: 20px 30px;
+  border-radius: 4px;
+  background-color: rgb(14, 95, 122);
+  font-size: 18px;
+}
+.btn:hover {
+  box-shadow: 6px 4px gray;
+}
+</style>
